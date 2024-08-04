2150005650d3a0724e788cf5024cd0eba16007ac
maxim <bozzhik@ya.ru>
2024-08-04 17:48:36 +0300

    init `Input` UI component based on `shadcn/ui`
    pnpm dlx shadcn-ui@latest add input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import {Input} from '#/UI/Input'
+import {cn} from './lib/utils'
+
+const websiteBox = 'max-w-2xl xl:max-w-xl mx-auto sm:mx-4'
+
 function App() {
-  return <h1 className="text-3xl font-semibold">Hello world!</h1>
+  return (
+    <main className={cn('mt-10 sm:mt-5', websiteBox)}>
+      <form className="space-y-3">
+        <Input placeholder="Вставьте ссылку с YouTube" />
+      </form>
+    </main>
+  )
 }
 
 export default App
