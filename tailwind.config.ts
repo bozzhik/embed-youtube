256cb0e456c6a34ddc94834cc5eb778e481fee79
maxim <bozzhik@ya.ru>
2024-08-04 17:33:03 +0300

    install `shadcn/ui`
    https://ui.shadcn.com/docs/installation/vite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,33 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type {Config} from 'tailwindcss'
+
+const config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  prefix: '',
   theme: {
-    extend: {},
     screens: {
       xl: {max: '1536px'},
       lg: {max: '1024px'},
       sm: {max: '768px'},
       xs: {max: '350px'},
     },
+    extend: {
+      keyframes: {
+        'accordion-down': {
+          from: {height: '0'},
+          to: {height: 'var(--radix-accordion-content-height)'},
+        },
+        'accordion-up': {
+          from: {height: 'var(--radix-accordion-content-height)'},
+          to: {height: '0'},
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config
+
+export default config
