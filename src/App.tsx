5377c52febfa4c5e849770f91e019fd2db5f7435
maxim <bozzhik@ya.ru>
2024-08-04 17:58:22 +0300

    init `Button` UI component based on `shadcn/ui`
    pnpm dlx shadcn-ui@latest add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import {Input} from '#/UI/Input'
 import {cn} from './lib/utils'
+import {Input} from '#/UI/Input'
+import {Button} from '#/UI/Button'
 
 const websiteBox = 'max-w-2xl xl:max-w-xl mx-auto sm:mx-4'
 
@@ -8,6 +9,10 @@ function App() {
     <main className={cn('mt-10 sm:mt-5', websiteBox)}>
       <form className="space-y-3">
         <Input placeholder="Вставьте ссылку с YouTube" />
+
+        <Button className="w-full" type="submit">
+          Добавить видео
+        </Button>
       </form>
     </main>
   )
