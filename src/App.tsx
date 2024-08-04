bed7ccc32e314dc8987b85ec5e3118e9024ccb8b
maxim <bozzhik@ya.ru>
2024-08-04 18:05:53 +0300

    add `queue` of YouTube videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import {useState, ChangeEvent, FormEvent} from 'react'
+
 import {cn} from './lib/utils'
 import {Input} from '#/UI/Input'
 import {Button} from '#/UI/Button'
@@ -5,15 +7,39 @@ import {Button} from '#/UI/Button'
 const websiteBox = 'max-w-2xl xl:max-w-xl mx-auto sm:mx-4'
 
 function App() {
+  const [videoUrl, setVideoUrl] = useState<string>('')
+  const [embedUrl, setEmbedUrl] = useState<string>('')
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setVideoUrl(event.target.value)
+  }
+
+  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const videoId = videoUrl.split('v=')[1]?.split('&')[0]
+    if (videoId) {
+      setEmbedUrl(`https://www.youtube.com/embed/${videoId}`)
+    } else {
+      alert('Invalid YouTube URL')
+    }
+  }
+
   return (
-    <main className={cn('mt-10 sm:mt-5', websiteBox)}>
-      <form className="space-y-3">
-        <Input placeholder="Вставьте ссылку с YouTube" />
+    <main className={cn('mt-10 sm:mt-5 space-y-6', websiteBox)}>
+      <form className="space-y-3" onSubmit={handleFormSubmit}>
+        <Input value={videoUrl} onChange={handleInputChange} placeholder="Вставьте ссылку с YouTube" />
 
         <Button className="w-full" type="submit">
           Добавить видео
         </Button>
       </form>
+      {embedUrl && (
+        <section className="space-y-4">
+          <div className="rounded-md overflow-hidden">
+            <iframe className="w-full h-full aspect-video" width="560" height="315" src={embedUrl} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title="YouTube Video"></iframe>
+          </div>
+        </section>
+      )}
     </main>
   )
 }
